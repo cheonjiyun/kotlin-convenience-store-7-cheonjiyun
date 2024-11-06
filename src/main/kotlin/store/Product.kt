8cbd940a1ffd80303acfca342bb93574b9bf81af
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-06 22:04:38 +0900

    feat<Product>: 재고 수량보다 많은 수량을 구매하면, 예외를 발생시킨다

--- src/main/kotlin/store/Product.kt
+++ src/main/kotlin/store/Product.kt
@@ -6,6 +6,17 @@ class Product(private var quantity : Int) {
         return quantity
     }
     fun buy(quantity : Int){
+        checkCanBuyQuantity(quantity)
         this.quantity -= quantity
+    }
+
+    fun canBuyQuantity(quantity: Int): Boolean{
+        return quantity > this.quantity
+    }
+
+    private fun checkCanBuyQuantity(quantity : Int){
+        if (canBuyQuantity(quantity)){
+            throw IllegalStateException("재고 수량을 넘어갔습니다")
+        }
     }
 }
