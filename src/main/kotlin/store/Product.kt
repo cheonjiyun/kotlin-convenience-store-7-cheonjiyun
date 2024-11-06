c53e582556cbcf566ed3cd895f3f012c50ae8e35
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-11-06 22:23:50 +0900

    feat<Product>: 상품 객체에 이름을 받는다

--- src/main/kotlin/store/Product.kt
+++ src/main/kotlin/store/Product.kt
@@ -1,21 +1,27 @@
 package store
 
-class Product(private var quantity : Int) {
+// 상품
+class Product(private var name: String, private var quantity: Int) {
 
-    fun getQuantity() : Int{
+    fun getName(): String {
+        return name
+    }
+
+    fun getQuantity(): Int {
         return quantity
     }
-    fun buy(quantity : Int){
+
+    fun buy(quantity: Int) {
         checkCanBuyQuantity(quantity)
         this.quantity -= quantity
     }
 
-    fun canBuyQuantity(quantity: Int): Boolean{
+    fun canBuyQuantity(quantity: Int): Boolean {
         return quantity > this.quantity
     }
 
-    private fun checkCanBuyQuantity(quantity : Int){
-        if (canBuyQuantity(quantity)){
+    private fun checkCanBuyQuantity(quantity: Int) {
+        if (canBuyQuantity(quantity)) {
             throw IllegalStateException("재고 수량을 넘어갔습니다")
         }
     }
--- src/test/kotlin/store/ProductTest.kt
+++ src/test/kotlin/store/ProductTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 class ProductTest {
     @Test
     fun `상품을 구매하면, 구매한 수량만큼 재고가 줄어든다`() {
-        val product = Product(10)
+        val product = Product("상품이름", 10)
         product.buy(3)
 
         assert(product.getQuantity() == 10 - 3)
@@ -15,7 +15,7 @@ class ProductTest {
     @Test
     fun `재고 수량보다 많은 수량을 구매하면, 예외가 발생한다`() {
         assertThrows<IllegalStateException> {
-            val product = Product(10)
+            val product = Product("상품이름", 10)
             product.buy(11)
         }
     }
